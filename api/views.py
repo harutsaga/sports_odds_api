f90cee03bb6bbbb9ae0c09a5f868f1bad73f1c30
Jalil <jalilmarkel1992@gmail.com>
2022-02-12 02:46:01 -0500

    added create help md python

--- api/views.py
+++ api/views.py
@@ -18,7 +18,7 @@ def index(request):
     return render(request, 'index.html')
 
 class BookieView(APIView):
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.AllowAny, )
 
     @swagger_auto_schema(operation_description="Get all supported bookies.", operation_id='get_bookies', responses={200: openapi.Response('Bookie names', schema=openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Schema(type=openapi.TYPE_STRING)))})
     def get(self, request):
@@ -28,29 +28,32 @@ class BookieView(APIView):
         return Response(_list, status=status.HTTP_200_OK)
 
 class SportsView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    @swagger_auto_schema(operation_description="Get all supported sports types.", operation_id='get_sports', responses={200: openapi.Response('Sports type names', schema=openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Schema(type=openapi.TYPE_STRING)))})
     def get(self, request):
         _list = []
         for item in SportsType.choices:
             _list.append(item[0])
         return Response(_list, status=status.HTTP_200_OK)
         
-class EventView(APIView):
-    def get(self, request):
-        record = Event.objects.filter(startTime__gte=datetime.utcnow()).order_by('-startTime')
-        # record = Event.objects.filter(league=League.MLB).order_by('-league')
-        serial = EventSerializer(record, many=True)
-        return Response(serial.data, status=status.HTTP_200_OK)
 
 class MarketView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    @swagger_auto_schema(operation_description="Get all supported market types.", operation_id='get_markets', responses={200: openapi.Response('Market type names', schema=openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Schema(type=openapi.TYPE_STRING)))})
     def get(self, request):
         _list = []
         for item in MarketType.choices:
             _list.append(item[0])
         return Response(_list, status=status.HTTP_200_OK)
 
-class SelectionView(APIView):
-    def get(self, request):
-        record = Event.objects.filter(startTime__gte=datetime.utcnow()).order_by('-league')
-        # record = Event.objects.filter(league=League.MLB).order_by('-league')
-        serial = BookieLinkSerializer(record, many=True)
-        return Response(serial.data, status=status.HTTP_200_OK)
+class EventView(generics.ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = EventSerializer
+    queryset = Event.objects.filter(startTime__gte=datetime.utcnow()).order_by('-startTime')
+
+class SelectionView(generics.ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = BookieLinkSerializer
+    queryset = Event.objects.filter(startTime__gte=datetime.utcnow()).order_by('-league')
