b01bf2ba9198989cfe9315e35a228d8008921004
Jalil <jalilmarkel1992@gmail.com>
2022-02-18 06:05:23 -0500

    deploy bat

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,8 @@
 sh stop_docker.bat
 docker-compose up -d backend
 docker-compose exec -u root backend sh -c "chown -R user:root /home/user/app"
-docker-compose exec -u root backend sh -c "chown -R user:root /home/user/frontend/media"
 docker-compose exec -u root backend sh -c "chown -R user:root /home/user/staticfiles"
-docker-compose exec -u root backend sh -c "chown -R user:root /home/user/upload"
-docker-compose exec -u root backend sh -c "chmod -R 777 /home/user/frontend/media"
 docker-compose exec -u root backend sh -c "chmod -R 777 /home/user/staticfiles"
-docker-compose exec -u root backend sh -c "chmod -R 777 /home/user/upload"
 docker-compose exec -u root backend python manage.py collectstatic --noinput --clear
 docker-compose exec -u root backend python manage.py makemigrations
 docker-compose exec -u root backend python manage.py migrate
